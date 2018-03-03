2fb372d9d531fe825e495d5d94c443544257647c
WeirdBubble <36774804+WeirdBubble@users.noreply.github.com>
2018-03-03 22:15:50 +0300

    Add files via upload

--- index.ts
+++ index.ts
@@ -1,42 +1,48 @@
-import prompt from '@jace1995/prompt';
+import readline from '@jace1995/readline';
 
-enum operator {'+','-','/', '*'};
+enum Operator {plus, div, minus, multiply};
 
-class Generation{
+interface OperatorLabels {
+    [key: number]: string;
+}
+
+const operatorLabels: OperatorLabels = {
+    [Operator.plus]: '+', 
+    [Operator.div]: '/', 
+    [Operator.minus]: '-', 
+    [Operator.multiply]: '*'
+};
+
+class Generation {
     private first : number;
     private second : number;
-    private task : operator;
+    private task : Operator;
 
-    constructor ()
-    {
-        this.first=this.randomNumber();
-        this.second=this.randomNumber();
-        this.task=this.randomOperator();
+    constructor () {
+        this.first = this.randomNumber();
+        this.second = this.randomNumber();
+        this.task = this.randomOperator();
     } 
 
-    private randomOperator(): operator
-    {
-        return <operator> Math.round(Math.random()*4);
+    private randomOperator(): Operator {
+        return <Operator> Math.round(Math.random()*4);
     }    
-    private randomNumber(): number
-    {
+    private randomNumber(): number {
         const res=Math.round(Math.random()*20-10);
         return res? res: 10;
     }
 
-    toString()
-    {
-        return `${this.first} ${operator[this.task]} ${this.second} = `;
+    toString() {
+
+        return `${this.first} ${operatorLabels[this.task]} ${this.second} = `;
     }
 
-    calculate()
-    {
-        switch(this.task)
-        {
-            case operator["+"]: return this.first+this.second;
-            case operator["-"]: return this.first-this.second;
-            case operator["/"]: return this.first/this.second;
-            case operator["*"]: return this.first*this.second;
+    calculate() {
+        switch(this.task) {
+            case Operator.plus: return this.first+this.second;
+            case Operator.minus: return this.first-this.second;
+            case Operator.div: return this.first/this.second;
+            case Operator.multiply: return this.first*this.second;
         }
     }
 
@@ -45,16 +51,14 @@ class Generation{
 
 async function main ()
 {
-   while(true)
-   {
+   while(true) {
        const operation = new Generation();
-       const answer = await prompt(operation.toString() + '\n');
-       if(+answer != +operation.calculate().toFixed(1))
+       const answer = await readline(operation.toString() + '\n');
+       if( +answer != +operation.calculate().toFixed(1))
        {
            console.log('Хватит пить');
            break;
-       } else 
-       {
+       } else {
            console.log('Еще по одной');
        }
     }
